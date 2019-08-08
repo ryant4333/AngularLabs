0166ce035a6274f44f008ac8b5e452bdf56fb68d
Ryan <RyanCTaylor95@gmail.com>
2019-08-08 17:08:13 +1000

    Lab 4

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const users = [
+  {name: "ryant4333@hotmail.com", pwd: "1234"},
+  {name: "RyanCTaylor95@gmail.com", pwd: "1234"},
+  {name: "ryan.taylor5@griffithuni.edu.au", pwd: "1234"}
+]
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  firstName = '';
+  pass = '';
+  showErr = false;
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  itemClicked() {
+    for (let i = 0; i < users.length; i++) {
+      if (this.firstName == users[i].name && this.pass == users[i].pwd) {
+        console.log("Success")
+        this.router.navigateByUrl('/account');
+      }
+    }
+    this.showErr = true;
+  }
+
+}
