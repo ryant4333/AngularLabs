e623b78ceec624563d8e6852026f8d42dfe75cf8
Ryan <RyanCTaylor95@gmail.com>
2019-08-08 17:50:46 +1000

    adding server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const http = require('http').Server(app)
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '../dist/week4/')))
